e45b05873d8019a1f203f70c88f9b8ddb100b22c
Alex <alex@Alexs-Mac-mini.local>
2022-05-20 17:28:09 +1200

    update

--- src/DynamicForm/dynamicForm.tsx
+++ src/DynamicForm/dynamicForm.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
 import ChildForm from "./childForm";
-const DynamicForm: React.FC<{ formData: any}> = ({ formData }) => {
+const DynamicForm: React.FC<{ formData: any }> = ({ formData }) => {
   return (
     <div>
-      {Object.keys(formData).map((key:any) => {
-          return <ChildForm data = {formData[key]}/>
+      {Object.keys(formData).map((key: any) => {
+        switch (key) {
+          case "experienceField":
+            return <ChildForm data={formData[key]} />;
+          case "education":
+            return <ChildForm data={formData[key]} />;
+        }
       })}
     </div>
   );
